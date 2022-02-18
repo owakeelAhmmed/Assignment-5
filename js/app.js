411ab4550e9935e25a5219b5a6ff1e06b7bec9ac
owakeelahmmed <mdowakeelahmed5678@gamail.com>
2022-02-18 11:45:30 +0600

    modify to js file

--- js/app.js
+++ js/app.js
@@ -1,44 +1,63 @@
 
-
+let income = 0;
+let expense = 0;
 document.getElementById('calc-btn').addEventListener('click', function(){
-    const totalIncome = document.getElementById('income-input');
-    const totalAmount = totalIncome.value;
+    const incomeInput = document.getElementById('income-input');
+    const incomeAmount = incomeInput.value;//========income input fild ======>
+    income= parseFloat(incomeAmount);
     const foodInput = document.getElementById('food-input');
-    const foodAmount = foodInput.value;
+    const foodAmount = foodInput.value;//========foof input fild =======>
     const rentInput = document.getElementById('rent-input');
-    const rentAmont = rentInput.value;
+    const rentAmont = rentInput.value;//======== reent input fild ======>
      const clothesInput = document.getElementById('clothes-input');
-     const clothesAmount = clothesInput.value;
-     const expensesTotal = document.getElementById('total-expenses');
+     const clothesAmount = clothesInput.value;//=======clothes input fild ======>
+     const expensesTotal = document.getElementById('total-expenses');//===== expenses Total id =====>
      const expensesNewTotal = parseFloat(foodAmount) + parseFloat(rentAmont) + parseFloat(clothesAmount);
      expensesTotal.innerText = expensesNewTotal;
- 
- 
-     const totalBalnceText = document.getElementById('balance');
-     const newBalancetotal = totalAmount - expensesNewTotal;
-     totalBalnceText.innerText = newBalancetotal;
+     expense = parseFloat(expensesTotal.innerText);
+    //====== If the user gives negative input then this erroe will show ===========>
+     if(incomeAmount < 0){
+         alert("Please enter the positiv value!!")
+     }else if(foodAmount < 0){
+        alert("Please enter the positiv value!!")
+     }else if(rentAmont < 0){
+        alert("Please enter the positiv value!!")
+     }else if(clothesAmount < 0){
+        alert("Please enter the positiv value!!")
+     }
+     else{
+        const totalBalnceText = document.getElementById('balance');
+        const newBalancetotal = incomeAmount - expensesNewTotal;
+        totalBalnceText.innerText = newBalancetotal;
+     }
 
-     totalIncome.value = '';
+     incomeInput.value = '';
      foodInput.value = '';
      rentInput.value = '';
      clothesInput.value = '';
  });
 
-
  document.getElementById('Save-calc').addEventListener('click', function(){
-    const saveCalc = document.getElementById('save-money-input');
-    const saveAmount = saveCalc.value;
-    const savingTotal = document.getElementById('saving-amount');
-    const totalBalnceText = document.getElementById('balance');
-    const remainingBalancetext = document.getElementById('remaining-balance');
-    let previousBalanceText = parseFloat(totalBalnceText.innerText);
-    const savesMoney = (previousBalanceText * saveAmount) / 100;
-    savingTotal.innerText = savesMoney;
-
-
-    remainingBalance = parseFloat(balance.innerText) - savesMoney ;
-    remainingBalancetext.innerText = remainingBalance;
+    const saveMoneyInput = document.getElementById('save-money-input');
+    const saveAmount = saveMoneyInput.value;//===== Save money input =======>
+    const savingAmount = document.getElementById('saving-amount');// ====== Saving amount id =======>
+    const totalBalnce = document.getElementById('balance');// ======= Balance id ========>
+    const remainingBalancetext = document.getElementById('remaining-balance'); // remaining balance id =====>
+    let BalanceText = parseFloat(totalBalnce.innerText);
+    const savesMoney = (BalanceText * saveAmount) / 100;
   
+    //======> If the cost is more than the user's income then this error will show =======>
+    if(income<expense){
+       alert("expense more then income")
+    }else if(saveAmount < 0){
+        alert("Please enter the positiv value!!")
+    }
+    else{
+        remainingBalance = parseFloat(balance.innerText) - savesMoney;
+        savingAmount.innerText = savesMoney;
+        remainingBalancetext.innerText = remainingBalance;
+    }
+ 
 });
 
 
